6e7af63b442816c990058ea2e727f73439399a07
walterpinto00 <walterpinto0718@gmail.com>
2026-09-22 06:17:23 -0500

    feat: completado ejercicio ConsoleApp1

---
+++ Escritorio/Basicos/ConsoleApp1/ConsoleApp1/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Persona
+    {
+        //Atributos
+        public string nombre;
+        public int edad;
+    }
+}
