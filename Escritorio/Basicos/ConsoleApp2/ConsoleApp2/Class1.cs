ba0211b1120f2708635d259c806cedd601be163a
walterpinto00 <walterpinto0718@gmail.com>
2026-09-22 06:17:23 -0500

    feat: completado ejercicio ConsoleApp2

---
+++ Escritorio/Basicos/ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Class1
+    {
+        private string Nombre;
+        private int Edad;
+
+        public string codigo;
+
+
+        //Consttrctor
+        public Class1(String Nom,int Eda,string Codi)
+         {
+          Nombre=Nom;
+            Edad=Eda;
+            codigo=Codi;
+        }
+
+
+    }
+}
---
+++ Escritorio/Basicos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
