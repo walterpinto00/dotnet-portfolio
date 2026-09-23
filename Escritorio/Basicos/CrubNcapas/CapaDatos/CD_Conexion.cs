2b70cd2ac1bcf979ce714e957f05d8b231c22c77
walterpinto00 <walterpinto0718@gmail.com>
2026-09-23 06:15:26 -0500

    feat: completado ejercicio CrubNcapas

---
+++ Escritorio/Basicos/CrubNcapas/CapaDatos/CD_Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CapaDatos
+{
+    public class CD_Productos
+    {
+        CD_Conexion conexion = new CD_Conexion();
+
+        public DataTable Mostrar()
+        {
+            DataTable tabla = new DataTable();
+            using (SqlConnection con = conexion.AbrirConexion())
+            {
+                using (SqlCommand comando = new SqlCommand("MostrarProductos", con))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader leer = comando.ExecuteReader())
+                    {
+                        tabla.Load(leer);
+                    }
+                }
+            }
+            return tabla;
+        }
+    }
+}
---
+++ Escritorio/Basicos/CrubNcapas/CapaDatos/CD_Productos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace CapaDatos
+{
+    public class CD_Productos
+    {
+            
+            }
+
+        }
+    
