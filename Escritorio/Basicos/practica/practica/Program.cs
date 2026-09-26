d63ecf9eddc17066e302cbc480ff136d6f08fd7a
walterpinto00 <walterpinto0718@gmail.com>
2026-09-26 08:53:14 -0500

    feat: completado ejercicio practica

---
+++ Escritorio/Basicos/practica/practica/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            p.Nombre = "Ana";
+            p.saludar();
+        }
+    }
+}
---
+++ Escritorio/Basicos/practica/practica/persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practica
+{
+    internal class Persona
+    {
+        //Atributos
+        public string Nombre;
+
+        public void saludar()
+        {
+            Console.WriteLine($"hola soy {Nombre}");
+        }
+    }
+}
